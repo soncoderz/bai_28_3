00b53dacbdd992476247019e8c621b717ce79ae6
Trịnh Công Sơn <145648237+soncoderz@users.noreply.github.com>
2026-03-28 16:21:52 +0700

    Delete schemas directory

--- schemas/reservation.js
+++
@@ -1,9 +0,0 @@
-/*
-    user
-    itemReservation : product,quantity,title,price,promotion, subtotal
-    status: "actived","cancelled","expired","transfer",
-    expiredIn,
-    promotion
-    Amount,
-    idempotency
-*/
